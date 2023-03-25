bea7fe0bbbe307b562482a100fc9a742cc29daa6
wisersanch3z <wiserepic3@gmail.com>
2023-03-24 20:50:26 -0600

    cambios pequeños

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const client = new discord.Client({
 
 //NPMS
 const db = require('megadb');
-//NPMS  test bruhg
+//NPMS añadido
 
 //! CODIGO
 client.commands = new discord.Collection();
--- slashCommads/general/help.js
+++ slashCommads/general/help.js
@@ -7,5 +7,5 @@ module.exports = {
     
       interaction.reply({ content: "QUE TAL NO HAY COMANDOS JADJKADJKHADJKHDASJKHDFGWKFGWKHFGWIUFGEWIFGEWIYFGWEIFGWIUFWGIUFWE" })
       }
-    
+    //test bruhh
   };

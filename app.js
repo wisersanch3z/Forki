ba83b82c83533a30acd8235d0227a3d4092a4cfd
wisersanch3z <wiserepic3@gmail.com>
2023-03-24 23:17:22 -0600

    feat: hola fix: nada, orueba de mierda

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const client = new discord.Client({
 const db = require('megadb');
 //NPMS 
 
-//------- 
+//------- t
 client.commands = new discord.Collection();
 client.events = new discord.Collection();
 client.slash = new discord.Collection();

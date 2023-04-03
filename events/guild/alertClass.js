358cba9149895fa8a1e874084a9ca45756659a70
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-04-03 13:35:04 -0600

    F: arreglos en el STA, me cago en las 24h

--- events/guild/alertClass.js
+++ events/guild/alertClass.js
@@ -3,40 +3,36 @@ const Discord = require('discord.js');
 
 
 module.exports = (client, discord, interaction) => {
-    console.log(`SISTEMA DE ALARMA CARGADO`);
+   
 
-    const msg = "Mensaje de prueba: 1h class";
-  const hora = "2023-04-03T13:30:00.000-06:00";
+    
+  function enviarMensajeSoloViernes(msg, hora) {
+    // Calcula la hora actual en El Salvador
+    const ahora = new Date();
+    const horaElSalvador = new Date(ahora.getTime() - (6 * 60 * 60 * 1000));
   
-// Crea la función para enviar el mensaje solo los viernes
-function enviarMensajeSoloViernes(msg, hora, canal) {
-  // Calcula la hora actual en El Salvador
-  const ahora = new Date();
-  const horaElSalvador = new Date(ahora.getTime() - (6 * 60 * 60 * 1000));
-
-
+    // Comprueba si hoy es viernes
+    if (horaElSalvador.getDay() !== 5) { // 5 representa el número del día de la semana (0 = domingo, 1 = lunes, 2 = martes, etc.)
+      console.log(`No es viernes, el mensaje no será enviado.`);
+      return;
+    }
   
-  // Comprueba si hoy es viernes
-  if (horaElSalvador.getDay() !== 5) { // 5 representa el número del día de la semana (0 = domingo, 1 = lunes, 2 = martes, etc.)
-    console.log(`No es viernes, el mensaje no será enviado.`);
-    return;
+    // Calcula cuánto tiempo queda hasta la hora deseada en El Salvador
+    const horaDeseada = new Date(hora);
+    horaDeseada.setHours(horaDeseada.getHours() - 6);
+    const tiempoRestante = horaDeseada.getTime() - horaElSalvador.getTime();
+  
+    // Espera hasta la hora deseada antes de enviar el mensaje
+    setTimeout(() => {
+      // Encuentra el canal donde deseas enviar el mensaje
+      const canal = client.channels.cache.get('1092524838247219270');
+      if (!canal) return console.error('El canal no existe.');
+  
+      // Envía el mensaje
+      canal.send(msg);
+    }, tiempoRestante);
   }
+enviarMensajeSoloViernes('Mensaje enviado desde el STA(Sistema de Alarma) este mensaje debio enviarse a las 1:40', '2023-04-07T13:40:00.000-06:00');
 
-
-
-  // Calcula cuánto tiempo queda hasta la hora deseada en El Salvador
-  const horaDeseada = new Date(hora);
-  horaDeseada.setHours(horaDeseada.getHours() - 6);
-  const tiempoRestante = horaDeseada.getTime() - horaElSalvador.getTime();
-
-  // Espera hasta la hora deseada antes de enviar el mensaje
-  setTimeout(() => {
-    // Encuentra el canal donde deseas enviar el mensaje
-    const canalDiscord = "1092524838247219270";
-    if (!canalDiscord) return console.error('El canal no existe.');
-
-    // Envía el mensaje
-    canalDiscord.send(msg);
-  }, tiempoRestante);
-}
+console.log(`SISTEMA DE ALARMA CARGADO`);
 }

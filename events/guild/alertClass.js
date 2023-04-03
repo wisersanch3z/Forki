d7718c2fd22046af62ffc57dc56f948ff6cacbf6
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-04-03 13:26:59 -0600

    F: Mala declaración de la variable en el STA

--- events/guild/alertClass.js
+++ events/guild/alertClass.js
@@ -5,6 +5,9 @@ const Discord = require('discord.js');
 module.exports = (client, discord, interaction) => {
     console.log(`SISTEMA DE ALARMA CARGADO`);
 
+    const msg = "Mensaje de prueba: 1h class";
+  const hora = "2023-04-03T13:30:00.000-06:00";
+  
 // Crea la función para enviar el mensaje solo los viernes
 function enviarMensajeSoloViernes(msg, hora, canal) {
   // Calcula la hora actual en El Salvador
@@ -12,8 +15,7 @@ function enviarMensajeSoloViernes(msg, hora, canal) {
   const horaElSalvador = new Date(ahora.getTime() - (6 * 60 * 60 * 1000));
 
 
-  const msg = "Mensaje de prueba: 1h class";
-  const hora = "2023-04-03T13:30:00.000-06:00";
+  
   // Comprueba si hoy es viernes
   if (horaElSalvador.getDay() !== 5) { // 5 representa el número del día de la semana (0 = domingo, 1 = lunes, 2 = martes, etc.)
     console.log(`No es viernes, el mensaje no será enviado.`);

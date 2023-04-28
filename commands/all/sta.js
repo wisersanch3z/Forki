19489c6671d190053fdc0b971a2000c9fff6ab91
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-04-27 23:03:27 -0600

    N: Ping global en el STA

--- commands/all/sta.js
+++ commands/all/sta.js
@@ -19,31 +19,65 @@ if(!eleccion) return message.reply("Ingresa un numero del 1 al 4")
         const sociales = new discord.MessageEmbed()
     
         .setColor("BLUE")
-        .setFooter("[STA] | Sistema de Tortura Alarmista")
         .setDescription(
-"`Mensaje programado para las 1:00pm`\n\
+"`Mensaje programado para las 1:00pm | Viernes`\n\n\
 ⏰ | Link de Zoom para unirse a la clase de Sociales:\n[Click aqui para unirte](https://us05web.zoom.us/j/87340813135?pwd=bkxIMnB4QXZxYjNkL2RDVFVmdXNRdz09)\
-\n⏹ | Detalles del enlace:\n🆔 | ID de reunión: 873 4081 3135\n🔐 | Código de acceso: Sociales23")
+\n\n⏹ | **Detalles del enlace**:\n🆔 | ID de reunión: 873 4081 3135\n🔐 | Código de acceso: Sociales23")
+.setFooter("[STA] | Sistema de Tortura Alarmista")
+        client.channels.resolve('1101362256408760402').send({content: `<@&1101361745261502614>`, embeds: [sociales]})
 
-        client.channels.resolve('1092524838247219270').send({embeds: [sociales]})
+        message.reply("enviado - clase sociales")
         }
             
         if(eleccion == "2"){
 
             //HORA INFORMATICA
-        
-            message.reply("info")
+            const informatica = new discord.MessageEmbed()
+    
+            .setColor("RED")
+            .setDescription(
+    "`Mensaje programado para las 1:45pm | Viernes`\n\n\
+    ⏰ | Link de Zoom para unirse a la clase de Informática:\n[Click aqui para unirte](https://us04web.zoom.us/j/78561115699?pwd=kiGbfURj9e3sYAOdDnrUBWqNBTg2b2.1)\
+    \n\n⏹ | **Detalles del enlace**:\n🆔 | ID de reunión: 785 6111 5699\n🔐 | Código de acceso: INFO9-23")
+    .setFooter("[STA] | Sistema de Tortura Alarmista")
+            client.channels.resolve('1101362256408760402').send({content: `<@&1101361745261502614>`, embeds: [informatica]})
+
+            message.reply("enviado - clase informatica")
         }
 
         if(eleccion == "3"){
 
             //HORA ARTISTICA
+
+            const artistica = new discord.MessageEmbed()
+    
+            .setColor("ORANGE")
+            .setDescription(
+    "`Mensaje programado para las 2:30pm | Viernes`\n\n\
+    ⏰ | Link de Zoom para unirse a la clase de Artística:\n[Click aqui para unirte](https://us05web.zoom.us/j/81373137268?pwd=V3FEK29EdW1ZS2U3UjZWK0FPMjk1dz09)\
+    \n\n⏹ | **Detalles del enlace**:\n🆔 | ID de reunión: 813 7313 7268\n🔐 | Código de acceso: CCJSJ2023")
+    .setFooter("[STA] | Sistema de Tortura Alarmista")
+            client.channels.resolve('1101362256408760402').send({content: `<@&1101361745261502614>`, embeds: [artistica]})
+
+            message.reply("enviado - clase artistica")
             
         }
 
         if(eleccion == "4"){
 
             //HORA LENGUAJE
+
+            const lenguaje = new discord.MessageEmbed()
+    
+            .setColor("YELLOW")
+            .setDescription(
+    "`Mensaje programado para las 3:15pm | Viernes`\n\n\
+    ⏰ | Link de Zoom para unirse a la clase de Lenguaje:\n[Click aqui para unirte](https://us05web.zoom.us/j/86430265687?pwd=UERMOS91MXl4bkZrRHUvOGFoKzZFdz09)\
+    \n\n⏹ | **Detalles del enlace**:\n🆔 | ID de reunión: 864 3026 5687\n🔐 | Código de acceso: LENGUAJE23")
+    .setFooter("[STA] | Sistema de Tortura Alarmista")
+            client.channels.resolve('1101362256408760402').send({content: `<@&1101361745261502614>`, embeds: [lenguaje]})
+
+            message.reply("enviado - clase lenguaje")
             
         }
 

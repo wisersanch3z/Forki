774ea3b86bd23f894c477be60d071bf44cd55643
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-05-05 14:55:54 -0600

    N: Soporte a las caracteristicas de la v14

---
+++ Handlers/menuHandler.js
@@ -0,0 +1,16 @@
+async function loadbMenus(client) {
+  const { loadFiles } = require("../Functions/fileLoader");
+
+  await client.menus.clear();
+
+  const Files = await loadFiles("Menus");
+
+  Files.forEach((file) => {
+    const menu = require(file);
+    client.menus.set(menu.data.name, menu);
+  });
+
+  return console.log("\nMenus Loaded.");
+}
+
+module.exports = { loadbMenus };

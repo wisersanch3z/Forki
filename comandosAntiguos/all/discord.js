719e3f24136979d7bf7fd8c7ce647a24e08f9fe5
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-05-05 14:48:19 -0600

    Cambios de ubicación de algunos archivos (8)

---
+++ comandosAntiguos/all/discord.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "discord",
+    description: "te da el link de este discord",
+    aliases: ["ds"],
+   
+    async execute(client, message, args, discord) {
+    
+   message.reply("https://discord.gg/CxmGW7abTS\n\n ***unico link permanente del servidor.***")
+   
+   }
+   }

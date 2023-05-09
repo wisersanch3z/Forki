ef1132c8dde1dc738ff1b39c6f6c71455d8145b7
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-05-08 23:50:40 -0600

    CMD comentado

--- Commands/Public/repeatsystem.js
+++ Commands/Public/repeatsystem.js
@@ -31,19 +31,19 @@ const {
     
         if(valor < 0) return interaction.reply({content: "El valor colocado es menor a 0!"})
       
-      
+
         if(valor == 1){
 
     interaction.reply({content: "Haz activado mi repetidor!"})
   
             
-        let x = await repitSchema.findOne({Guild: interaction.guild.id}).exec()//Vemos si ya hay algo guardado
+        let x = await repitSchema.findOne({Guild: interaction.guild.id}).exec()
         if(x){ 
-        await x.updateOne({Guild: interaction.guild.id})//Si ya hay un canal guardado, actualizamos al nuevo canal.
+        await x.updateOne({Guild: interaction.guild.id})
     }
 
-    let i = new repitSchema({Guild: interaction.guild.id})//Colocamos los datos 
-    await i.save()//Guardamos los datos
+    let i = new repitSchema({Guild: interaction.guild.id})
+    await i.save()
 
     }
         
@@ -51,9 +51,9 @@ const {
     interaction.reply({content: "Haz desactivado mi repetidor!"})
 
 
-    let x = await repitSchema.findOneAndDelete({Guild: interaction.guild.id}).exec()//Vemos si ya hay algo guardado
+    let x = await repitSchema.findOneAndDelete({Guild: interaction.guild.id}).exec()
         if(x){ 
-        await x.updateOne({Guild: interaction.guild.id})//Si ya hay un canal guardado, actualizamos al nuevo canal.
+        await x.updateOne({Guild: interaction.guild.id})
     }
 
 

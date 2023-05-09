2857dd3c5c809ff557a403854a9b579cac8393d7
Wiser <99498581+wisersanch3z@users.noreply.github.com>
2023-05-08 23:09:08 -0600

    CMD para manipular el repetidor

---
+++ Commands/Public/repeatsystem.js
@@ -0,0 +1,64 @@
+const {
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+    EmbedBuilder,
+  } = require("discord.js");
+
+  const repitSchema = require('../../Schema/repitSchema')
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("repeatsystem")
+      .setDescription("Manipula el sistema de repetición de mensajes")
+      .addNumberOption(option =>
+        option.setName('valor')
+        .setDescription('0=ACTIVAR - 1=DESACTIVAR')
+        .setRequired(true)),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     */
+  
+
+    
+
+    async execute(interaction, client) {
+  
+
+        const valor = interaction.options.getNumber('valor');
+
+        if(valor > 1) return interaction.reply({content: "El valor que has colocado es mayor a 1!"})
+    
+        if(valor < 0) return interaction.reply({content: "El valor colocado es menor a 0!"})
+      
+      
+        if(valor == 0){
+
+    interaction.reply({content: "Haz activado mi repetidor!"})
+  
+            
+        let x = await repitSchema.findOne({Guild: interaction.guild.id}).exec()//Vemos si ya hay algo guardado
+        if(x){ 
+        await x.updateOne({Guild: interaction.guild.id})//Si ya hay un canal guardado, actualizamos al nuevo canal.
+    }
+
+    let i = new repitSchema({Guild: interaction.guild.id})//Colocamos los datos 
+    await i.save()//Guardamos los datos
+
+    }
+        
+           if(valor == 1){
+    interaction.reply({content: "Haz desactivado mi repetidor!"})
+
+
+    let x = await repitSchema.findOneAndDelete({Guild: interaction.guild.id}).exec()//Vemos si ya hay algo guardado
+        if(x){ 
+        await x.updateOne({Guild: interaction.guild.id})//Si ya hay un canal guardado, actualizamos al nuevo canal.
+    }
+
+
+           }
+        
+
+    }
+}
